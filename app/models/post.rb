c94e55374240bd23e8d7a690362236a39e838386
Jesse <jessewambu@zetech.ac.ke>
2023-03-14 10:23:37 +0300

    Added Soln

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :clickbait?
+  
+  #   CLICKBAIT_PATTERNS = [
+  #     "Won't Believe",
+  #     "Secret",
+  #     "Top ",
+  #     "Guess",
+  #   ]
+   
+  CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i,
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+  end
